cb10a78683ac67056f8e4016d63ce2ead1d236ba
nerovanity <hamani.dami.imad@gmail.com>
2025-01-17 13:33:24 +0100

    testing

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ihamani <ihamani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 10:20:00 by ihamani           #+#    #+#             */
-/*   Updated: 2025/01/17 11:37:35 by ihamani          ###   ########.fr       */
+/*   Updated: 2025/01/17 13:17:51 by ihamani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,20 +34,57 @@ t_list	*parse_stack(int ac, char **av)
 	return (stack);
 }
 
+void	sort2(t_list **stack_a)
+{
+	if ((*stack_a)->nbr > (*stack_a)->next->nbr)
+		sa(stack_a);
+}
+
+void	sort3(t_list **stack_a, t_list **stack_b)
+{
+	// edge cases
+	pb(stack_a, stack_b);
+	sort2(stack_a);
+	if ((*stack_b)->nbr > (*stack_a)->next->nbr)
+	{
+		pa(stack_a, stack_b);
+		ra(stack_a);
+	}
+	else
+	{
+		if ((*stack_b)->nbr < (*stack_a)->nbr)
+		{
+			pa(stack_a, stack_b);
+			ra(stack_a);
+			rra(stack_a);
+		}
+		else
+		{
+			pa(stack_a, stack_b);
+			sa(stack_a);
+		}
+	}
+}
+
+void	sort4(t_list **stack_a, t_list **stack_b)
+{
+	pb(stack_a, stack_b);
+	sort3(stack_a, stack_b);
+}
+
 int	main(int ac, char **av)
 {
 	t_list	*stack_a;
 	t_list	*stack_b;
 
 	stack_a = parse_stack(ac, av);
+	stack_b = NULL;
 	if (!stack_a)
 		p_error();
-	stack_b = ft_lstnew(3);
-	ra(&stack_a);
-	pa(&stack_a, &stack_b);
-	ra(&stack_a);
+	sort4(&stack_a, &stack_b);
 	print_stack(stack_a);
 	write(1, "stack b\n", 9);
-	print_stack(stack_b);
+	if (stack_b)
+		print_stack(stack_b);
 	free_stack(&stack_a);
 }
--- src/print_stack.c
+++ src/print_stack.c
@@ -6,7 +6,7 @@
 /*   By: ihamani <ihamani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 11:06:03 by ihamani           #+#    #+#             */
-/*   Updated: 2025/01/15 11:11:57 by ihamani          ###   ########.fr       */
+/*   Updated: 2025/01/17 12:10:21 by ihamani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	print_stack(t_list *head)
 {
+	if (head == NULL)
+		return ;
 	while (head)
 	{
 		printf("Number : %d\n", head->nbr);
